6e0d43cdae816ecb81a677f83eb3a8059e2f38e6
Liz Morrison <efmorr@gmail.com>
2015-03-25 12:59:13 -0400

    Add migrations, model, and controller for answers.

---
+++ app/models/answer.rb
@@ -0,0 +1,6 @@
+class Answer < ActiveRecord::Base
+  validates :description, presence: true, length: {minimum: 50}
+
+  belongs_to :question
+
+end
---
+++ db/migrate/20150325164540_create_answers_table.rb
@@ -0,0 +1,8 @@
+class CreateAnswersTable < ActiveRecord::Migration
+  def change
+    create_table :answers do |t|
+      t.text :description, null: false
+      t.integer :question_id, null: false
+    end
+  end
+end
