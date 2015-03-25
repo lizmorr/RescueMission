5fdadb983e3871be3c765aa94fa20d77d7a81412
Liz Morrison <efmorr@gmail.com>
2015-03-25 15:58:51 -0400

    Write tests for view answers feature

--- spec/features/user_views_all_answers_spec.rb
+++ spec/features/user_views_all_answers_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 feature "user views answers" do
+  scenario "answers_displayed" do
+    question1 = FactoryGirl.create(:question)
+    answer1 = FactoryGirl.create(:answer, question: question1)
+    answer2 = FactoryGirl.create(:answer, description: "x"*150, question: question1)
 
+    visit question_path(question1)
+
+    page.assert_selector('ul#all_questions li:first-child', text: answer2.description)
+    page.assert_selector('ul#all_questions li:last-child', text: answer1.description)
+
+  end
 end
