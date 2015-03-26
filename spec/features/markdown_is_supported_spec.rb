5e4322f7b02eb6cc9e07f3ce5e75d5b5aa343ba2
Liz Morrison <efmorr@gmail.com>
2015-03-26 17:46:36 -0400

    Add markdown

--- spec/features/markdown_is_supported_spec.rb
+++ spec/features/markdown_is_supported_spec.rb
@@ -4,18 +4,18 @@ feature 'markdown is supported' do
   scenario 'user submits question with title using markdown' do
     question = FactoryGirl.create(:question, title: "Hello! *This* is"+"z"*40)
     visit question_path(question)
-    expect(page).to have_css('#title_block i', text: "This")
+    expect(page).to have_css('#title_block em', text: "This")
   end
 
   scenario 'user submits question with details using markdown' do
     question = FactoryGirl.create(:question, detail: "*This* is detail"+"z"*150)
     visit question_path(question)
-    expect(page).to have_css('#detail_block i', text: "This")
+    expect(page).to have_css('#detail_block em', text: "This")
   end
 
   scenario 'user submits answer using markdown' do
     answer = FactoryGirl.create(:answer, description: "*Hello*"+"z"*120)
     visit question_path(answer.question)
-    expect(page).to have_css('#answer_block i', text: "Hello")
+    expect(page).to have_css('#answer_block em', text: "Hello")
   end
 end
