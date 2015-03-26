11aadf5b4a19f05311856384ca072be61c4455fa
Liz Morrison <efmorr@gmail.com>
2015-03-26 13:31:34 -0400

    Add tests for feature: user deletes question.

---
+++ spec/features/user_deletes_questions_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature 'user deletes question' do
+
+# As a user
+# I want to delete a question
+# So that I can delete duplicate questions
+#
+# Acceptance Criteria
+#
+# - I must be able delete a question from the question edit page
+# - I must be able delete a question from the question details page
+# - All answers associated with the question must also be deleted
+
+  scenario 'user deletes from question deletion page' do
+    question_to_keep = FactoryGirl.create(:question, title: "fd"*25)
+    question_to_delete = FactoryGirl.create(:question)
+
+    visit question_path(question_to_delete)
+    click_on 'Delete question'
+
+    expect(page).to have_no_content(question_to_delete.title)
+    expect(page).to have_content(question_to_keep.title)
+  end
+
+  scenario 'user deletes from question edit page' do
+    question_to_keep = FactoryGirl.create(:question, title: "fd"*25)
+    question_to_delete = FactoryGirl.create(:question)
+
+    visit edit_question_path(question_to_delete)
+    click_on 'Delete question'
+
+    expect(page).to have_no_content(question_to_delete.title)
+    expect(page).to have_content(question_to_keep.title)
+  end
+
+end
