5dcfe748fb4787d08c994ae1719c6d38e0e1b369
Liz Morrison <efmorr@gmail.com>
2015-03-26 15:58:39 -0400

    Add feature tests for supporting markdown.

---
+++ spec/features/markdown_is_supported_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'markdown is supported' do
+  scenario 'user submits question with title using markdown' do
+    question = FactoryGirl.create(:question, title: "Hello! *This* is"+"z"*40)
+    visit question_path(question)
+    expect(page).to have_css('#title_block i', text: "This")
+  end
+
+  scenario 'user submits question with details using markdown' do
+    question = FactoryGirl.create(:question, detail: "*This* is detail"+"z"*150)
+    visit question_path(question)
+    expect(page).to have_css('#detail_block i', text: "This")
+  end
+
+  scenario 'user submits answer using markdown' do
+    answer = FactoryGirl.create(:answer, description: "*Hello*"+"z"*120)
+    visit question_path(answer.question)
+    expect(page).to have_css('#answer_block i', text: "Hello")
+  end
+end
