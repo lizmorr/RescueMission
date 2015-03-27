567e8dbca6bffa56aa1d2339888d70b0e0014591
Liz Morrison <efmorr@gmail.com>
2015-03-27 13:50:50 -0400

    Add styling elements.

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 40}
   validates :detail, presence: true, length: {minimum: 150}
 
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
 end
